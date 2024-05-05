c45d32eed505556bf0126665d1cc08b5287cf107
riazahmedmahin <c201060@ugrad.iiuc.ac.bd>
2024-05-05 22:35:12 +0600

    commit

--- lib/presentation/ui/screens/welcome_screen.dart
+++ lib/presentation/ui/screens/welcome_screen.dart
@@ -53,6 +53,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               SizedBox(
                 height: 5,
               ),
+
               CustomTextButton(
                 onPressed: () {
                   Get.to(Search_locationScreen());
